948be2e0a78b52633526808f6870abffbccaf8f9
Dan <fnfbchd75@yahoo.com>
2026-02-22 19:17:37 +0000

    Adding extension classes

---
+++ ATC2027/ATC2027/ExtensionClasses/MathExtension.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATC2027.ExtensionClasses
+{
+    public static class MathExtension
+    {
+        public static float Map(float val, float min, float max) {
+            float result = 0;
+            
+            result = max - min;
+            result /= 100;
+            result *= val;
+            result += min;
+
+            return result;
+        }
+        public static float Map(double val, double min, double max)
+        {
+            double result = 0;
+
+            result = max - min;
+            result /= 100;
+            result *= val;
+            result += min;
+
+            return (float)result;
+        }
+    }
+}
