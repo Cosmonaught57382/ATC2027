bc543c1f440d78e7ec2ef94dcf5023f9cf2c86a6
Daniel <fnfbchd75@yahoo.com>
2026-02-27 10:42:22 +0000

    adding close control to the ATC_Library

---
+++ ATC2027/ATC2027/ATC_Library/Clearance.cs
@@ -0,0 +1,45 @@
+﻿using ATC2027.ATC_Library.Heading;
+using ATC2027.Library.Altitude;
+using ATC2027.Library.Speed;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATC2027.ATC_Library
+{
+    public class Clearance
+    {
+        IHeading? heading;
+        IAltitude? altitude;
+        ISpeed? speed;
+
+        public Clearance(IHeading? heading = null, IAltitude? altitude = null, ISpeed? speed = null)
+        {
+            this.heading = heading;
+            this.altitude = altitude;
+            this.speed = speed;
+        }
+
+        public Clearance ApplyHeading(IHeading heading)
+        {
+            this.heading = heading;
+            return this;
+        }
+
+        public Clearance ApplyAltitude(IAltitude altitude)
+        {
+            this.altitude = altitude;
+            return this;
+        }
+
+        public Clearance ApplySpeed(ISpeed speed)
+        {
+            this.speed = speed;
+            return this;
+        }
+
+        public Clearance getNullClearance => new Clearance(null, null, null);
+    }
+}
