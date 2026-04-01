fd741c266f85a53c95ea3a423c1f58020723dff6
Daniel Cook <fnfbchd75@yahoo.com>
2026-04-01 19:00:05 +0100

    updating extension classes

--- ATC2027/ATC2027/ExtensionClasses/MathExtension.cs
+++ ATC2027/ATC2027/ExtensionClasses/MathExtension.cs
@@ -2,6 +2,17 @@
 {
     public static class MathExtension
     {
+        public static bool IsANumberWithInRange(string text, float lowerLimitInclusive, float upperLimitInclusive)
+        {
+            float f;
+            try
+            {
+                f = float.Parse(text);
+
+                return lowerLimitInclusive <= f && f <= upperLimitInclusive;
+            }
+            catch { return false; }
+        }
         public static float Map(float yAsAPercentageAcrossXDomain, float smallXValue, float bigXValue) {
             float result = 0;
             
